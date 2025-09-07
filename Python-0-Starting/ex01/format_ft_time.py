6a470465a3b90cad00266752740f85e22f3c1108
alsaeed3 <ali.eissa.570@gmail.com>
2025-09-08 02:16:18 +0400

    Add initial implementation of time formatting in format_ft_time.py

---
+++ Python-0-Starting/ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+import time
+#import datetime
+
+# print(time.asctime())
+# print(time.ctime())
+# print(time.localtime())
+# print(time.mktime())
+print("Seconds since January 1, 1970: ", time.time(), " or ", )
