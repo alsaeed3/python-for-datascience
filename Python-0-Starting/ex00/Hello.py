dca86dd223a150e8934c04d36f6c7b0d7ad1ca03
alsaeed3 <ali.eissa.570@gmail.com>
2025-09-07 17:52:17 +0400

    Refactor data structure examples in Hello.py for clarity and correctness

--- Python-0-Starting/ex00/Hello.py
+++ Python-0-Starting/ex00/Hello.py
@@ -1,23 +1,23 @@
+#list is an ordered and mutable collection of elements, supports duplicates and direct changing
 ft_list = ["Hello", "tata!"]
+#tuple is an ordered and immutable collection, elements can't be changed after creation
 ft_tuple = ("Hello", "toto!")
+#set is an unordered and unique collection, doesn't support direct changing, we need to remove first
 ft_set = {"Hello", "tutu!"}
+#dict (dictionary) is an ordered and mutable collection of key-value pairs, supports direct changing
 ft_dict = {"Hello" : "titi!"}
 
 #your code here
-#list is an ordered and mutable collection of elements, supports duplicates and changing.
 #we can access the elements by their index in the list and update them directly
-ft_list[1] = "World"
+ft_list[1] = "World!"
 
-#tuple is an ordered and immutable collection, elements can't be changed after creation
 #to change an element we need to overwrite the whole content of a tuple
 ft_tuple = ("Hello", "United Arab Emirates!")
 
-#set is an unordered and unique collection
 #if we need to update an element, we have to remove it and add the new element
 ft_set.remove("tutu!")
 ft_set.add("Abu Dhabi!")
 
-#dict is dictionary which is a mutable collection of key-value pairs
 #we can update the key value, add a new key-value and delete a key and access key value safely.
 ft_dict["Hello"] = "42AbuDhabi!"
 
